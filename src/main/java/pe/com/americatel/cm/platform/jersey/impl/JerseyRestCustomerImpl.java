0ee56ae1b6e8c221018b3f573cdf72ce61dff53e
SistemasAmtel <45668176+SistemasAmtel@users.noreply.github.com>
2018-12-06 12:46:58 -0500

    Update JerseyRestCustomerImpl.java

--- src/main/java/pe/com/americatel/cm/platform/jersey/impl/JerseyRestCustomerImpl.java
+++ src/main/java/pe/com/americatel/cm/platform/jersey/impl/JerseyRestCustomerImpl.java
@@ -34,7 +34,9 @@ public class JerseyRestCustomerImpl implements JerseyRestCustomer {
 		Response response = null;
 
 		String httpMethod = "POST";
-		String urlEndpoint = "http://190.187.252.105/backend-sig-app/api/endpoint/customer/resources/post/customerByAccountManager";
+		//String urlEndpoint = "http://190.187.252.105/backend-sig-app/api/endpoint/customer/resources/post/customerByAccountManager";
+		String urlEndpoint = "http://190.187.252.105/backend-sig-app/api/endpoint/customer/resources/post/customerByAccountManager;
+					
 		Map<String, Object> httpHeaders = new HashMap<String, Object>();
 		httpHeaders.put("Content-Type", "application/json");
 
